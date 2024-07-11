cbfdca059fcc6cba0c8935ee28be99cf4f085faa
iamlydial <lydia.lavecchia@gmail.com>
2024-07-11 13:00:21 -0500

    style header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, CategoryNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
