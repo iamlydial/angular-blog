b35358050203a62d02aa62415c55e5b391ca57f7
iamlydial <lydia.lavecchia@gmail.com>
2024-07-11 14:15:39 -0500

    subscription component
    add markup,bootstrap and custom styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
-import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
-import { PostCardComponent } from "./layouts/post-card/post-card.component";
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, CategoryNavbarComponent, PostCardComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CategoryNavbarComponent,
+    PostCardComponent,
+    SubscriptionFormComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-blog';
