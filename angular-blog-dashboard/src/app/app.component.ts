0eec49b1be9bab072db84e3fe352513e38a9709a
iamlydial <lydia.lavecchia@gmail.com>
2024-07-19 14:39:21 -0500

    add dashboard component with category card and fontawesome icons

--- angular-blog-dashboard/src/app/app.component.ts
+++ angular-blog-dashboard/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
 import { FooterComponent } from "./layouts/footer/footer.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
