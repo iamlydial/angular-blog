cffcbae1277defc3c6a109bc42da92b6c8c97529
iamlydial <lydia.lavecchia@gmail.com>
2024-07-19 13:00:22 -0500

    add comment form markup and style

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { CommentFormComponent } from '../../comments/comment-form/comment-form.component';
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommentFormComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
