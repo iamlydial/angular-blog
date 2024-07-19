33a0ab59e27d885253fd8238f802c20c8b2071d6
iamlydial <lydia.lavecchia@gmail.com>
2024-07-19 12:35:43 -0500

    add single postcard markup and style

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
