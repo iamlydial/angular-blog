d66b3bb195bbce63efb1048a2b1e67b857384629
iamlydial <lydia.lavecchia@gmail.com>
2024-07-24 11:40:01 -0400

    Update firebase.config.ts

--- angular-blog-dashboard/src/app/firebase.config.ts
+++ angular-blog-dashboard/src/app/firebase.config.ts
@@ -1,6 +1,4 @@
-import { EnvironmentProviders, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
