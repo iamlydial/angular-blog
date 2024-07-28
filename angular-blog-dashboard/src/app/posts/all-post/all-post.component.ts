f3b90947183576a6897557efa5e7d735c3e57df1
iamlydial <lydia.lavecchia@gmail.com>
2024-07-28 18:06:43 -0400

    add components
    - all post
    - new post
    components

---
+++ angular-blog-dashboard/src/app/posts/all-post/all-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-post.component.html',
+  styleUrl: './all-post.component.css'
+})
+export class AllPostComponent {
+
+}
