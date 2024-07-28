53ff9d999b4051f207d872804c558e8bc0dbfe7f
iamlydial <lydia.lavecchia@gmail.com>
2024-07-28 19:21:11 -0400

    loads categories
    - add category services
    - add load function to load categories
    - add for loop in template to showcase them in the option field

--- angular-blog-dashboard/src/app/posts/new-post/new-post.component.ts
+++ angular-blog-dashboard/src/app/posts/new-post/new-post.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CategoriesService } from '../../services/categories.service';
+import { Category } from '../../models/category';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css',
 })
@@ -12,9 +15,15 @@ export class NewPostComponent implements OnInit {
   permalink: string = '';
   imgSrc: any = './assets/placeholder-image.png';
   selectedImage: any;
+  categories: Array<Category> = [];
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private categoryService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe((val) => {
+      this.categories = val;
+    });
+  }
 
   onTitleChanges($event: any) {
     const title = $event.target.value;
