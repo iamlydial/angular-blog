59c39af400e313d9f3568e77a04d11645b9c5767
iamlydial <lydia.lavecchia@gmail.com>
2024-07-28 18:19:13 -0400

    add router links for all post buttons

--- angular-blog-dashboard/src/app/posts/all-post/all-post.component.ts
+++ angular-blog-dashboard/src/app/posts/all-post/all-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-all-post',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './all-post.component.html',
   styleUrl: './all-post.component.css'
 })
