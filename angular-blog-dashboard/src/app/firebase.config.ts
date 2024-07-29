3627c23a757ef32fee4bedd3c6546c2353e1b3f3
iamlydial <lydia.lavecchia@gmail.com>
2024-07-29 13:00:54 -0400

    add firebase storage
    - install npm package
    - modify the firebase providers by adding firebase stoarage to the existin ones

--- angular-blog-dashboard/src/app/firebase.config.ts
+++ angular-blog-dashboard/src/app/firebase.config.ts
@@ -1,8 +1,10 @@
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 
 export const firebaseProviders = [
   provideFirebaseApp(() => initializeApp(environment.firebase)),
   provideFirestore(() => getFirestore()),
+  provideStorage(() => getStorage()),
 ];
