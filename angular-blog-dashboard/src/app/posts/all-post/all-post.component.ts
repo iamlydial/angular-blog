730684698e6feb310c04f2a24b6ac38623bff4ae
iamlydial <lydia.lavecchia@gmail.com>
2024-07-30 10:49:48 -0400

    add table with post template
    - add a function to format time

--- angular-blog-dashboard/src/app/posts/all-post/all-post.component.ts
+++ angular-blog-dashboard/src/app/posts/all-post/all-post.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Post } from '../../models/post';
 import { PostsService } from '../../services/posts.service';
+import { CommonModule } from '@angular/common';
+import { Timestamp } from '@firebase/firestore';
 
 @Component({
   selector: 'app-all-post',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './all-post.component.html',
   styleUrl: './all-post.component.css',
 })
 export class AllPostComponent implements OnInit {
   posts: Array<Post> = [];
+
+  
 
   constructor(private postService: PostsService){
 
@@ -19,6 +23,18 @@ export class AllPostComponent implements OnInit {
   ngOnInit(): void {
     this.postService.loadData().subscribe((postVal) => {
       this.posts = postVal;
+    });
+  }
+
+  formatDate(timestamp: Timestamp | Date): string {
+    const date = timestamp instanceof Timestamp ? timestamp.toDate() : timestamp;
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
     });
   }
 }
