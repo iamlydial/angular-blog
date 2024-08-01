eaee91b32424fb92bcbf361515ccc48155cd31f1
iamlydial <lydia.lavecchia@gmail.com>
2024-08-01 11:45:19 -0400

    Update auth.service.ts

--- angular-blog-dashboard/src/app/services/auth.service.ts
+++ angular-blog-dashboard/src/app/services/auth.service.ts
@@ -6,19 +6,25 @@ import {
   createUserWithEmailAndPassword,
   signOut,
   User,
+  authState,
 } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  user$: Observable<User | null>; 
+
   constructor(
     private afAuth: Auth,
     private toastr: ToastrService,
     private router: Router
-  ) {}
+  ) {
+    this.user$ = authState(this.afAuth); /
+  }
 
   async login(email: string, password: string): Promise<User | null> {
     try {
@@ -32,6 +38,17 @@ export class AuthService {
       return userCredential.user;
     } catch (error: any) {
       this.toastr.warning(error);
+      return null;
+    }
+  }
+
+  async loadUser(): Promise<User | null> {
+    const user = this.afAuth.currentUser;
+    if (user) {
+      console.log(JSON.parse(JSON.stringify(user))); // Log user details
+      return user;
+    } else {
+      console.log('No user is signed in.');
       return null;
     }
   }
