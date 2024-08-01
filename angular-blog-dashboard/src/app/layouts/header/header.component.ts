ea3a099f4fe6b8d2446cb9f405623398e025bfd1
iamlydial <lydia.lavecchia@gmail.com>
2024-08-01 16:26:48 -0400

    Update header.component.ts
    add  isPlatformBrowser  to ensure localStorage is only accessed in the browser environment.

--- angular-blog-dashboard/src/app/layouts/header/header.component.ts
+++ angular-blog-dashboard/src/app/layouts/header/header.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Observable, Subscription } from 'rxjs';
 import { RouterLink } from '@angular/router';
 
@@ -16,16 +22,22 @@ export class HeaderComponent implements OnInit {
   isLoggedIn$!: Observable<boolean>;
   private subscription: Subscription = new Subscription();
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit(): void {
-    const user = localStorage.getItem('user');
-    if (user) {
-      const parsedUser = JSON.parse(user);
-      this.emailUser = parsedUser.email; // Access the email property
-      console.log(this.emailUser, 'user email from local storage');
-    } else {
-      console.log('No user found in local storage');
+    if (isPlatformBrowser(this.platformId)) {
+      // Access localStorage only in the browser
+      const user = localStorage.getItem('user');
+      if (user) {
+        const parsedUser = JSON.parse(user);
+        this.emailUser = parsedUser.email; // Access the email property
+        console.log(this.emailUser, 'user email from local storage');
+      } else {
+        console.log('No user found in local storage');
+      }
     }
 
     this.isLoggedIn$ = this.authService.isLoggedIn();
