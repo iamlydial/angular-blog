09f059581884b49a419ad73a98d8ef3e7bcd9630
iamlydial <lydia.lavecchia@gmail.com>
2024-08-06 19:54:14 +0200

    Update single-category.component.ts
    - activateRoute to get the id

--- angular-blog-frontend/src/app/pages/single-category/single-category.component.ts
+++ angular-blog-frontend/src/app/pages/single-category/single-category.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-category',
   standalone: true,
   imports: [PostCardComponent],
   templateUrl: './single-category.component.html',
-  styleUrl: './single-category.component.css'
+  styleUrl: './single-category.component.css',
 })
-export class SingleCategoryComponent {
+export class SingleCategoryComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((val) => {
+      console.log(val);
+    });
+  }
 
+  ngOnInit(): void {}
 }
