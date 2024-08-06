7e13152b8be83597c8e84ccae9c538941bcc0842
iamlydial <lydia.lavecchia@gmail.com>
2024-08-06 19:50:40 +0200

    add routerLink to

--- angular-blog-frontend/src/app/layouts/category-navbar/category-navbar.component.ts
+++ angular-blog-frontend/src/app/layouts/category-navbar/category-navbar.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../../services/categories.service';
 import { Category } from '../../../../../angular-blog-dashboard/src/app/models/category';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './category-navbar.component.html',
   styleUrl: './category-navbar.component.css'
 })
