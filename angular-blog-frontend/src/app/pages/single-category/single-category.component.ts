2114f2291f3a3566e6c5a4fda67edeef79e3555f
iamlydial <lydia.lavecchia@gmail.com>
2024-08-06 19:59:14 +0200

    load category specific posts
    - add subscription to loadcategorypost service
    - add the post loop to the template

--- angular-blog-frontend/src/app/pages/single-category/single-category.component.ts
+++ angular-blog-frontend/src/app/pages/single-category/single-category.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 import { ActivatedRoute } from '@angular/router';
+import { PostsService } from '../../services/posts.service';
+import { Post } from '../../../../../angular-blog-dashboard/src/app/models/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-category',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommonModule],
   templateUrl: './single-category.component.html',
   styleUrl: './single-category.component.css',
 })
 export class SingleCategoryComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {
+  postsArray!: Array<Post>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService
+  ) {
     this.route.params.subscribe((val) => {
       console.log(val);
+      this.postService.loadCategoryPost(val['id']).subscribe((post) => {
+        this.postsArray = post;
+      });
     });
   }
 
